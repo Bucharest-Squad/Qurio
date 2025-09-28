9fc7492bf8c51bd0fbe00b97ac08594247721f06
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-09-28 14:34:14 +0300

    rollback main activity

--- app/src/main/java/com/bucharest/qurio/MainActivity.kt
+++ app/src/main/java/com/bucharest/qurio/MainActivity.kt
@@ -1,36 +1,20 @@
 package com.bucharest.qurio
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.bucharest.qurio.components.ResultDialog
-import com.bucharest.qurio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(binding.root)
-        setUpInsets()
-        ResultDialog(
-            correct = 5,
-            incorrect = 2,
-            skipped = 1,
-            stars = 3
-        ).show(supportFragmentManager, "result_dialog")
-    }
-
-    private fun setUpInsets(){
+        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 }
