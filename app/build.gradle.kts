047693d339b4e997cc73da10cc68ad85977ac4ca
AimanYosofi <AimanYosofiUlt@gmail.com>
2025-10-16 18:11:03 +0300

    implement loading layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    implementation(libs.glide)
+
     // Navigation
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
